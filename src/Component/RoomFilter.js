efa32cb80a666dbab335f2683572dac83d183a4b
unknown <ldhung9901@gmail.com>
2020-06-19 16:39:29 +0700

    Revert "ver4"
    This reverts commit 500d00e11ae8498be877af21ff8b70b373d641de.

--- src/Component/RoomFilter.js
+++ src/Component/RoomFilter.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { Roomcontext } from "../context";
 import Title from "./Title";
 const getUnique = (items, value) => {
+ 
   return [...new Set(items.map((item) => item[value]))];
 };
 export default function RoomFilter({ rooms }) {
@@ -52,8 +53,8 @@ export default function RoomFilter({ rooms }) {
           >
             {types}
           </select>
-        </div>
-        <div className="form-group">
+          </div>
+          <div className="form-group">
           <label htmlFor="capacity">Capacity</label>
           <select
             name="capacity"
@@ -64,68 +65,9 @@ export default function RoomFilter({ rooms }) {
           >
             {peoples}
           </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="price">Price ${price}</label>
-          <input
-            name="price"
-            id="price"
-            type="range"
-            min={minPrice}
-            max={maxPrice}
-            value={price}
-            className="form-control"
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div className="form-group">
-          <label>Size </label>
-          <input
-            name="minSize"
-            id="minSize"
-            type="number"
-            min={minSize}
-            max={maxSize}
-            value={minSize}
-            className="size-input"
-            onChange={handleChange}
-          ></input>
-          <input
-            name="maxSize"
-            id="maxSize"
-            type="number"
-            min={minSize}
-            max={maxSize}
-            value={maxSize}
-            className="size-input"
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div className="form-group">
-          <div className="single-extra">
-            <input
-              type="checkbox"
-              name="breakfast"
-              id="breakfast"
-              checked={breakfast}
-              className="single-extra"
-              onChange={handleChange}
-            ></input>
-            <label htmlFor="breakfast">breakfast</label>
           </div>
-
-          <div className="single-extra">
-            <input
-              type="checkbox"
-              name="pets"
-              id="pets"
-              checked={pets}
-              className="single-extra"
-              onChange={handleChange}
-            ></input>
-            <label htmlFor="pets">pets</label>
-          </div>
-        </div>
+        
+        {}
       </form>
     </section>
   );
